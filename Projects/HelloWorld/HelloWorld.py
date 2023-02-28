a00a3f779c2e22489e86e76498fb56898eccca94
Vico <victor.chapula@gmail.com>
2023-02-27 23:28:08 -0600

    Session 27/02/2023

---
+++ Projects/HelloWorld/HelloWorld.py
@@ -0,0 +1,2 @@
+print("Hello World!!")
+print("First String", "Second String", 40)
---
+++ Projects/HelloWorld/Strings.py
@@ -0,0 +1,9 @@
+
+print("printing 'single' quotes")
+print('Printing "double" quotes')
+print("Hello" + " " + 'World')
+
+fName = "Vico"
+lName = input("Whats your last name?: ")
+
+print("The name is:", fName + " " + lName)
